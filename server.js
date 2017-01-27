71a9a5bae83318ca3f088b3e3cbc98de7bf8322b
StreakInTheSky <streakinthesky@gmail.com>
2017-01-27 00:09:56 -0800

    add PUT to update posts

--- server.js
+++ server.js
@@ -64,6 +64,34 @@ app.post('/posts', (req, res) => {
     });
 });
 
+app.put('/posts/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    res.status(400).json({message: message});
+  }
+
+  const toUpdate = {};
+  const updateableFields = ['title', 'author', 'content'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  })
+
+  Post
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .exec()
+    .then(posts => res.status(201).json(posts.apiRepr()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
+
 
 
 let server;
